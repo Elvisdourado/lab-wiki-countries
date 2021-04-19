1782d57c96ae45a65b41be0a1ccb9ad1862bc69e
Elvis Dourado <elvisdourado@gmail.com>
2021-04-18 23:19:47 -0300

    50%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+import NavBar from './components/Navbar'
+import CountriesList from './components/CountriesList'
+import CountryDetails from './components/CountryDetails'
+import countries from './countries.json';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div className="container">
+        <div className="row">
+          <CountriesList exact path="/" countries={countries} />
+          <Router>
+            <Route
+              path="/countries/:countryCode" component={CountryDetails}
+            />
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
